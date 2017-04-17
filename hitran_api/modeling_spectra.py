93998a5e22226e20c710e9d0e4eb528cb1b05bd8
artfin <artfin@mail.ru>
2017-04-17 14:00:37 +0300

    hitran spectra

--- hitran_api/modeling_spectra.py
+++ hitran_api/modeling_spectra.py
@@ -8,10 +8,11 @@ db_begin('data')
 
 for plot_number, temperature in enumerate([150.0, 200.0, 250.0, 300.0]):
     nu, coef = absorptionCoefficient_Lorentz(SourceTables='OH', Environment={'p':1.0, 'T': temperature})
+    nu, absorp = absorptionSpectrum(nu, coef, Environment={'l':100.0, 'T': temperature})
     ax = plt.subplot(2, 2, plot_number + 1)
     ax.set_title('T= ' + str(temperature) + 'K')
     ax.set_xlim(25, 600) 
-    plt.plot(nu, coef)
+    plt.plot(nu, absorp)
 
 plt.show()
 #plt.savefig('spectra/oh.png')
