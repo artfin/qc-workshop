cc8c635056c4c543e59226093d6800ecc75746bd
artfin <artfin@mail.ru>
2017-04-19 12:53:00 +0300

    merged plotter for rovibrational

--- scripts/rovibrational_spectra.py
+++ scripts/rovibrational_spectra.py
@@ -1,3 +1,16 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+def save_spectra(filename, transitions_m, transitions_p):
+    with open(filename, mode = 'w') as out:
+        out.write('transitions corresponding to j = +1')
+        for e in transitions_p:
+            out.write(str(e) + '\n')
+        out.write('='*30)
+        out.write('transitions corresponding to j = -1')
+        for e in transitions_m:
+            out.write(str(e) + '\n')
+
 filename = '/home/artfin/Desktop/repos/qc-workshop/data/my_potential/energy.dat'
 with open(filename, mode = 'r') as inputfile:
     lines = inputfile.readlines()
@@ -22,11 +35,18 @@ transitions_m = []
 for en1, en2 in zip(v0[1:], v1):
     transitions_m.append(en2 - en1)
 
-with open('rovibrational12.dat', mode = 'w') as out:
-    out.write('transitions corresponding to j = +1')
-    for e in transitions_p:
-        out.write(str(e) + '\n')
-    out.write('='*30)
-    out.write('transitions corresponding to j = -1')
-    for e in transitions_m:
-        out.write(str(e) + '\n')
+height_p = [1] * len(transitions_p)
+height_m = [1] * len(transitions_m)
+
+width = 3
+
+plt.bar(transitions_p, height_p, width, color = "blue")
+plt.bar(transitions_m, height_m, width, color = "red")
+
+red_patch = mpatches.Patch(color = 'red', label = 'dJ = -1')
+blue_patch = mpatches.Patch(color = 'blue', label = 'dJ = +1')
+
+plt.legend(handles = [red_patch, blue_patch])
+
+plt.show()
+
