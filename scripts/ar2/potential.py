75ba5b201b6040f5a1fa6364d13a75a5ee918196
artfin <artfin@mail.ru>
2017-06-04 16:52:39 +0300

    before pull

--- scripts/ar2/potential.py
+++ scripts/ar2/potential.py
@@ -1,6 +1,10 @@
 from __future__ import print_function
 from pprint import pprint
 
+import matplotlib.pyplot as plt
+
+htocm =  219474.63 
+
 def is_number(s):
     try:
         float(s)
@@ -8,29 +12,39 @@ def is_number(s):
     except ValueError:
         return False
 
-with open('../../molpro/tables/ar2_ci.tab', mode = 'r') as inputfile:
-    lines = inputfile.readlines()
+def read_potential(filename):
+    with open(filename, mode = 'r') as inputfile:
+        lines = inputfile.readlines()
 
-dist = []
-energy = []
+    dist = []
+    energy = []
 
-for line in lines:
-    if len(line) > 1:
-        data = line.split()
+    for line in lines:
+        if len(line) > 1:
+            data = line.split()
         
-        if len(data) < 1:
-            continue
+            if len(data) < 1:
+                continue
 
-        if is_number(data[0]):
-            dist.append(float(data[0]))
-            energy.append(float(data[1]))
+            if is_number(data[0]):
+                dist.append(float(data[0]))
+                energy.append(float(data[1]))
 
-htocm =  219474.63 
-energy = [(e - min(energy)) * htocm for e in energy]
+    return dist, energy
 
-for r, e in zip(dist, energy):
-    print(r, e)
+dist_ci, potential_ci = read_potential('ar2_ci.dat')
+dist_ccsd, potential_ccsd = read_potential('aug-cc-pvqz.dat')
+dist_avtz, potential_avtz = read_potential('avtz.dat')
 
-with open('ar2_ci.dat', mode = 'w') as out:
-    for r, e in zip(dist, energy):
-       out.write(str(r) + ' ' + str(e) + '\n')
+fig = plt.figure()
+plt.rc('text', usetex = True)
+
+lw = 1.5
+plt.plot(dist_ci, potential_ci, color = '0.6', linestyle = 'solid', linewidth = lw)
+plt.plot(dist_ccsd, potential_ccsd, color = '0.5', linestyle = 'dashed', linewidth = lw)
+plt.grid(linestyle = ':', alpha = 0.7)
+plt.show()
+
+#with open('ar2_ci.dat', mode = 'w') as out:
+    #for r, e in zip(dist, energy):
+       #out.write(str(r) + ' ' + str(e) + '\n')
