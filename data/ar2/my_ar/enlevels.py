5b81947c829d7240287a3d130cdbf1b34f28a5e3
artfin <artfin@mail.ru>
2017-06-05 23:27:33 +0300

    before merge

--- data/ar2/my_ar/enlevels.py
+++ data/ar2/my_ar/enlevels.py
@@ -12,6 +12,8 @@ with open('energy.dat', mode = 'r') as inputfile:
     lines = inputfile.readlines()
 
 v0 = []
+v1 = []
+v2 = []
 
 for line in lines:
     if len(line) > 0:
@@ -20,8 +22,17 @@ for line in lines:
         if is_number(data[0]):
             if float(data[0]) == 0:
                 v0.append(float(data[1]))
+            
+            if float(data[0]) == 1:
+                v1.append(float(data[1]))
 
-pprint(v0[::2])
+            if float(data[0]) == 2:
+                v2.append(float(data[1]))
+
+v2 = [s - v0[0] for s in v2]
+v1 = [s - v0[0] for s in v1]
 v0 = [s - v0[0] for s in v0]
 pprint(v0[::2])
+pprint(v1[::2])
+pprint(v2[::2])
             
--- scripts/ar2/potential.py
+++ scripts/ar2/potential.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 from pprint import pprint
 
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+import numpy as np
 
 htocm =  219474.63 
 
@@ -12,7 +14,7 @@ def is_number(s):
     except ValueError:
         return False
 
-def read_potential(filename):
+def read_potential(filename, dim):
     with open(filename, mode = 'r') as inputfile:
         lines = inputfile.readlines()
 
@@ -23,26 +25,88 @@ def read_potential(filename):
         if len(line) > 1:
             data = line.split()
         
-            if len(data) < 1:
+            if len(data) < 2:
                 continue
 
             if is_number(data[0]):
                 dist.append(float(data[0]))
                 energy.append(float(data[1]))
+    
+    if dim == 1:
+        print('Limit: {0}'.format(energy[-1]))
+        energy = [(e - energy[-1]) * htocm for e in energy]
+    else:
+        print('Limit: {0}'.format(energy[-1]))
+        energy = [e - energy[-1] for e in energy]
 
     return dist, energy
 
-dist_ci, potential_ci = read_potential('ar2_ci.dat')
-dist_ccsd, potential_ccsd = read_potential('aug-cc-pvqz.dat')
-dist_avtz, potential_avtz = read_potential('avtz.dat')
+def read_energy(filename, level):
+    
+    with open(filename, mode = 'r') as inputfile:
+        lines = inputfile.readlines()
+
+    v0 = []
+    for line in lines:
+        if len(line) > 0:
+            data = line.split()
+
+            if is_number(data[0]):
+                if int(data[0]) == level:
+                    v0.append(float(data[1]))
+
+    return v0
+
+dist_ccsd, potential_ccsd = read_potential('../../molpro/tables/ar2_ccsd.tab', dim = 1)
+dist_emp, potential_emp = read_potential('../../data/ar2/pec.dat', dim = 0)
+dist_patkowski, potential_patkowski = read_potential('../../data/ar2/patkowski/aug-cc-pv5z.dat', dim = 0)
+dist_slavcek, potential_slavcek = read_potential('../../data/ar2/slavcek/aug-cc-pv5z.dat', dim = 0)
+
+v0 = read_energy('../../data/ar2/my_ar/energy.dat', level = 0)
+v1 = read_energy('../../data/ar2/my_ar/energy.dat', level = 1)
+v2 = read_energy('../../data/ar2/my_ar/energy.dat', level = 2)
 
 fig = plt.figure()
 plt.rc('text', usetex = True)
+ax = plt.subplot(1, 1, 1)
+
+
+print(dist_ccsd)
+ccsd = interp1d(dist_ccsd, potential_ccsd, kind = 'cubic')
+emp = interp1d(dist_emp, potential_emp, kind = 'cubic')
+pat = interp1d(dist_patkowski, potential_patkowski, kind = 'cubic')
+slav = interp1d(dist_slavcek, potential_slavcek, kind = 'cubic')
+
+x = np.linspace(3.3, 8, num = 100, endpoint = True)
 
 lw = 1.5
-plt.plot(dist_ci, potential_ci, color = '0.6', linestyle = 'solid', linewidth = lw)
-plt.plot(dist_ccsd, potential_ccsd, color = '0.5', linestyle = 'dashed', linewidth = lw)
-plt.grid(linestyle = ':', alpha = 0.7)
+l1, = ax.plot(x, ccsd(x), color = '0.5', linestyle = 'dashed', linewidth = lw)
+l3, = ax.plot(x, pat(x), color = '0.4', linestyle = 'dotted', linewidth = lw)
+l4, = ax.plot(x, slav(x), color = '0.7', linestyle = 'solid', linewidth = lw)
+
+ax.scatter(dist_ccsd, potential_ccsd, marker = 'x', color = 'k')
+ax.scatter(dist_slavcek, potential_slavcek, color = 'k', marker = '*')
+ax.scatter(dist_patkowski, potential_patkowski, color = 'k', marker = 's')
+
+ax.set_xlim(3, 5)
+ax.set_ylim(-100, 10)
+
+#for index, e0, e1, e2 in zip(range(len(v0)), v0, v1, v2):
+    #ax.axhline(y = e0 + min(potential_ccsd), color = 'k', xmin = 0.12, xmax = 0.22)
+    #ax.axhline(y = e1 + min(potential_ccsd), color = 'k', xmin = 0.10, xmax = 0.29)
+    #ax.axhline(y = e2 + min(potential_ccsd), color = 'k', xmin = 0.09, xmax = 0.36)
+
+fig.legend((l1, l3, l4), (r'CCSD(T); \textit{aug-cc-pVQZ}', r'CCSD(T); \textit{aug-cc-pV5Z+spdfg}; K.Patkowski, G. Murdachaew, C. Fou and K. Szalewicz, Mol. Phys. \textbf{103}, 15, 2005', r'CCSD(T) \textit{aug-cc-pV5Z + spdfg}; including core electrons; P. Slavicek, R. Kalus, P. Paska, I. Odvarkova, O. Hobza and A. Malijevsky, J. Chem. Phys., \textbf{119}, 4, 2003'), 'lower center', ncol = 1, fancybox = True, shadow = True, prop = {'size': 'small'})
+
+#fig.legend((l1, l2), (r'CCSD(T); \textit{aug-cc-pVQZ}', r'\textit{Empirical}; E.A. Colbourn and A. E. Douglas,  J. Chem. Phys., \textbf{65}, 5, 1976'), 'lower center', ncol = 3, fancybox = True, shadow = True, prop = {'size': 'large'})
+
+ax.set_title(r'\textbf{Potential profiles}')
+ax.grid(linestyle = ':', alpha = 0.7)
+ax.set_xlabel(r'\textbf{r}, (bohr)')
+ax.set_ylabel(r'\textbf{U}, (cm$^{-1}$)')
+
+plt.tight_layout(pad = 1.7, w_pad = 0.0, h_pad = -3.0)
+
 plt.show()
 
 #with open('ar2_ci.dat', mode = 'w') as out:
